be82a983a094516d57efb8ca955e6589d54cc073
coolerputt01 <olumideadekolu9@gmail.com>
2025-11-24 12:48:30 +0100

    Setup

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<ncurses.h>
+
+
+int main(){
+	initscr();
+	cbreak();
+	noecho();
+	keypad(stdscr,TRUE);
+	printw("Hello TUI");
+	refresh();
+	getch();
+	endwin();
+	
+	return 0;
+}
